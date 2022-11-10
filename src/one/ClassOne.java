af4d7e41eb495ad05e42fa3f1e22e3efb3c37d5d
AtiqNazari <117694820+AtiqNazari@users.noreply.github.com>
2022-11-09 20:06:18 -0500

    chagnes added by Atiq

--- src/one/ClassOne.java
+++ src/one/ClassOne.java
@@ -3,6 +3,19 @@ package one;
 public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello world, this is for the class one practice ");
+		
+		
 	}
 
 }
