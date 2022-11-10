c81108d9e506007b5b96ed960ea178d6e9f908ed
Atiq Nazari <atiqullah.nazari@tekschool.us>
2022-11-09 19:54:08 -0500

    new changes

--- src/one/ClassOne.java
+++ src/one/ClassOne.java
@@ -3,6 +3,7 @@ package one;
 public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("hello world, this is for the class one practice ");
+		System.out.println("hello this is new added codes");
 	}
 
 }
