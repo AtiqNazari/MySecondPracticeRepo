5db738e85df0817078c6cc8335c84cf0bee648f2
Atiq Nazari <atiqullah.nazari@tekschool.us>
2022-11-09 19:23:24 -0500

    this is first commit

---
+++ src/three/ClassTwo.java
@@ -0,0 +1,8 @@
+package three;
+
+public class ClassTwo {
+	public void ClassTwo() {
+		System.out.println("this is class two ");
+	}
+
+}
---
+++ src/two/ClassThree.java
@@ -0,0 +1,9 @@
+package two;
+
+public class ClassThree {
+	public void ThisIsForClassTHree() {
+		System.out.println("this is for the class three practice");
+		
+	}
+
+}
